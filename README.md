29c7c11d28f032469c92df340e69c343383db946
jayxtt999@126.com <a13487612005>
2023-04-06 14:46:23 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,94 @@
 # blinker_esp8266_pc_power
 blinker控制电脑开机
+
+## 所需材料
+ - 继电器底板智能插座WIFI模块
+ - esp01s
+ - 线材若干，开关若干
+ - binker app
+
+![image](https://raw.githubusercontent.com/jayxtt999/blinker_esp8266_pc_power/main/img/esp01s.jpg)
+
+![image](https://raw.githubusercontent.com/jayxtt999/blinker_esp8266_pc_power/main/img/Snipaste_2023-04-06_13-33-40.jpg)
+## 开始
+
+
+
+### 接线
+`建议先烧录好程序，app端调试成功后在做接线调整`
+
+首先需要找到你电脑主板对应开关供电的接口，可以去品牌主板查询，如
+![image](https://raw.githubusercontent.com/jayxtt999/blinker_esp8266_pc_power/main/img/zhuban.jpg)
+`PWR_BTN` 就是开关机按钮，你随便拿个螺丝刀短接一下，看能否开机就可以了。一般是短接1s开关机，短接3秒强制关机
+
+
+将原来的开关线做简单改造
+原来应该是开关线（PWR GUD）直连一个前置面板的自锁电源开关
+
+![image](https://raw.githubusercontent.com/jayxtt999/blinker_esp8266_pc_power/main/img/pc.jpg)
+
+你可以把这条线的连接直接接入继电器模块，等于将自锁开关的控制权移交到继电器，但是不建议直接这样，我们需要前置面板的开关和继电器都能使用
+
+那么可以在原来的接线基础上新增2条线，接入继电器，如图示
+
+![image](https://raw.githubusercontent.com/jayxtt999/blinker_esp8266_pc_power/main/img/jiantu01.jpg)
+
+
+
+
+
+### 修改烧录程序
+
+#### Blinker
+ - 下载Blinker app
+ - 新增设备
+ - 为设备新增一个按钮，用于控制继电器开关
+ - 为设备新增一个文本，用于显示开机状态
+![image](https://raw.githubusercontent.com/jayxtt999/blinker_esp8266_pc_power/main/img/blink.jpg)
+
+### 代码
+
+在main.ino中修改
+```
+char auth[] = "";//Blinker key
+
+char ssid[] = "";//你的wifi名称
+char pswd[] = "";//你的wufi密码
+
+BlinkerButton Button1("btn-power");//btn-power为你新增按钮的组件键名
+#define TEXTE_1 "tex-power" //tex-power为你新增按钮的组件键名
+
+```
+
+### 测试
+
+![image](https://raw.githubusercontent.com/jayxtt999/blinker_esp8266_pc_power/main/img/blink2.jpg)
+`尝试开关是否有反应`
+
+
+### 代码说明
+ - 代码中开启了一个tcp服务用于与其它模块通信，比如你不想用app进行控制，当然你也可以使用http服务，需要注意不要暴露到公网。
+ - 开机状态，因为使用ping指令来当前电脑的开机状态，所以连接的wifi与电脑使用的需要在同一局域网，因为是ip，你需要在路由器中对电脑静态分配一个ip（mac绑定），否则可能有判断问题
+ - 这部分的代码高低电平切换才开关机，你需要自行测试一下，理论上应该只要置高或者置低就行，可能是我电脑的问题，或者有知道的可以说一下原因，情况是单置高会在3s后关机
+ ```
+    digitalWrite(0, 0);
+    delay(1000);
+    digitalWrite(0, 1);
+ ```
+
+ ### 供电问题
+
+此项目我使用的供电为在usb面板取点，刚好是5v。主板可能需要开启关机时usb供电，具体操作可以百度`电脑关机时usb供电`
+
+
+### 其它方案
+
+检测开机状态：你可以找下主板的供电位置，比如存在3v的。用开发板的引脚去检测高地电平（注意降压，否则可能烧板），关机低电平，开机高电平，这个比较准确（其实ping也还可以，就是存在功耗问题，你可以修改代码中对ping间隔时间的逻辑）
+供电：可以整一个18650电池，然后esp01s可以开启休眠模式降低功耗，再上一个充电模块当电脑在开机状态下为电池充电
+
+
+### TODO
+- [ ] 与天问开发板语音交互
+
+### 相关参考
+https://blog.csdn.net/CDL_LuFei/article/details/124496972
---
+++ src/main.ino
@@ -0,0 +1,131 @@
+#define BLINKER_WIFI
+
+#include <Blinker.h>
+#include <ESP8266Ping.h>
+
+char auth[] = "";//Blinker 秘钥
+char ssid[] = "TP-LINK_xxxxxx";//你的wifi名称
+char pswd[] = "";//你的wufi密码
+#define PCIP "192.168.6.100"//你的电脑ip地址
+uint32_t read_time = 0;
+uint16_t pc_status = 0;
+String power_msg = "";
+uint32_t gap_time = 200000; // 默认检测时长，20分钟检测一次
+// 新建组件对象
+BlinkerButton Button1("btn-power"); //btn-power为你新增按钮的组件键名
+BlinkerText Text1("tex-power");//tex-power为你新增按钮的组件键名
+WiFiServer server(6000);//开启一个tcp服务用于第三方控制，你也可以调整为http服务
+
+// 按下按键即会执行该函数
+void button1_callback(const String &state)
+{
+    BLINKER_LOG("get button state: ", state);
+    power();
+}
+
+//高低电平切换
+void power()
+{
+    digitalWrite(0, 0);
+    delay(1000);
+    digitalWrite(0, 1);
+    if (pc_status == 1)
+    {
+        power_msg = "关机中";
+    }
+    else
+    {
+        power_msg = "开机中";
+    }
+    Text1.print("状态:", power_msg);
+    // 人工点击，缩短默认检测时间为8s
+    gap_time = 8000;
+}
+
+// 如果未绑定的组件被触发，则会执行其中内容
+void dataRead(const String &data)
+{
+    BLINKER_LOG("Blinker readString: ", data);
+    Blinker.vibrate();
+    uint32_t BlinkerTime = millis();
+    Blinker.print("millis", BlinkerTime);
+}
+
+void setup()
+{
+    // 初始化串口
+    Serial.begin(115200);
+    // 初始化有LED的IO
+    pinMode(0, OUTPUT);
+    // BLINKER_DEBUG.stream(Serial);
+    // BLINKER_DEBUG.debugAll();
+    // 初始化blinker
+    Blinker.begin(auth, ssid, pswd);
+    Blinker.attachData(dataRead);
+    Button1.attach(button1_callback);
+    // 启动时检测一次
+    powerStatus();
+
+    server.begin();
+}
+
+//状态检测
+void powerStatus()
+{
+
+    if (Ping.ping(PCIP))
+    {
+        pc_status = 1;
+        power_msg = "已开机";
+    }
+    else
+    {
+        pc_status = 0;
+        power_msg = "已关机";
+    }
+    Text1.print("状态:", power_msg);
+}
+
+void loop()
+{
+    Blinker.run();
+    unsigned long current_time = millis();
+    if (read_time == 0 || (current_time - read_time) >= gap_time)
+    {
+        read_time = current_time;
+        gap_time = 200000;
+        powerStatus();
+    }
+    //这里当tcp连接收到POWER\r时，触发响应
+    WiFiClient client = server.available();
+    if (client)
+    {
+        BLINKER_LOG("New client connected");
+        while (client.connected())
+        {
+            if (client.available())
+            {
+                String request = client.readStringUntil('\r');
+                BLINKER_LOG(request);
+                // 处理客户端发送的数据
+                if (request.indexOf("POWER") != -1)
+                {
+                    power();
+                    powerStatus();
+                    BLINKER_LOG("POWER Success");
+                    client.println("Success");
+                }
+                else
+                {
+                    BLINKER_LOG("POWER Success");
+                    client.println("Error");
+                }
+                break;
+            }
+        }
+
+        client.stop();
+        BLINKER_LOG("Client disconnected");
+    }
+
+}
